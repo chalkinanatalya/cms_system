e6ec4cb95613cccfe18d29041194d3c0da142ca3
Natalya <m_d.r.lecter@protonmail.com>
2022-08-31 01:11:40 +0400

    tabs input

--- technoshop-cms/script/index.js
+++ technoshop-cms/script/index.js
@@ -1,13 +1,19 @@
 import { modalBtn, modal } from './elems.js';
 import { modalController } from './modalController.js';
 import { previewController } from './previewController.js';
+import {tableController } from './tableController.js';
 
 
-modalController({
-    modal, 
-    btn: modalBtn, 
-    classOpen: 'd-block', 
-    classClose: 'btn-close'
-})
+const init = () => {
+    modalController({
+        modal, 
+        btn: modalBtn, 
+        classOpen: 'd-block', 
+        classClose: 'btn-close'
+    })
+    
+    previewController();
+    tableController();
+}
 
-previewController();
+init();
